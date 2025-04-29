601e46c215f22d90ffa47699eaf5a1ae3a8cd8c7
vrouker <vrouker@gmail.com>
2025-04-29 19:55:56 -0400

    set up navbar and search bar (not working)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import bookLogo from './assets/books.png'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
+import Navigations from './components/Navigations'
 
 function App() {
   const [token, setToken] = useState(null)
@@ -8,6 +9,17 @@ function App() {
   return (
     <>
       <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
+
+      <div id="routeSection">
+        <Navigations className="navBar"/>
+        <Routes>
+          <Route path="/books" element={<h1>Books go here!</h1>}/>
+          <Route path="/books/:id" element={<h1>A single book should show here!</h1>}/>
+          <Route path="/account" element={<h1>User account goes here!</h1>}/>
+          <Route path="/login" element={<h1>Login page goes heres!</h1>}/>
+          <Route path="/register" element={<h1>Register form goes here</h1>}/>
+        </Routes>
+      </div>
 
       <p>Complete the React components needed to allow users to browse a library catalog, check out books, review their account, and return books that they've finished reading.</p>
 
