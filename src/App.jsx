2ae0f1537087538866153fd0ebd40a6cf7168a25
vrouker <vrouker@gmail.com>
2025-04-29 21:00:54 -0400

    created the all books view and successfully maps over all books from the API

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import bookLogo from './assets/books.png'
 import { Routes, Route, Link } from 'react-router-dom'
 import Navigations from './components/Navigations'
+import Books from './components/Books'
 
 function App() {
   const [token, setToken] = useState(null)
+  const [singleBook, setSingleBook] = useState({})
 
   return (
     <>
@@ -12,20 +14,15 @@ function App() {
 
       <div id="routeSection">
         <Navigations className="navBar"/>
+        <Books/>
         <Routes>
-          <Route path="/books" element={<h1>Books go here!</h1>}/>
+          <Route path="/books" element={<Books setSingleBook={setSingleBook}/>}/>
           <Route path="/books/:id" element={<h1>A single book should show here!</h1>}/>
           <Route path="/account" element={<h1>User account goes here!</h1>}/>
           <Route path="/login" element={<h1>Login page goes heres!</h1>}/>
           <Route path="/register" element={<h1>Register form goes here</h1>}/>
         </Routes>
       </div>
-
-      <p>Complete the React components needed to allow users to browse a library catalog, check out books, review their account, and return books that they've finished reading.</p>
-
-      <p>You may need to use the `token` in this top-level component in other components that need to know if a user has logged in or not.</p>
-
-      <p>Don't forget to set up React Router to navigate between the different views of your single page application!</p>
     </>
   )
 }
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,10 +1,40 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Books(){
+export default function Books({setSingleBook}){
+    const [allBooks, setAllBooks] = useState([]);
+
+    const navigate = useNavigate();
+    
+    useEffect(()=>{
+        const getAllBooks = async()=>{
+            const res = await fetch ("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books")
+
+            const results = await res.json();
+            setAllBooks(results);
+        } 
+        getAllBooks();
+        console.log(allBooks);
+    },[setAllBooks])
+
+    const handleClick =(book)=>{
+        setSingleBook(book);
+        navigate("/books/:id")
+    }
 
     return(
-
+        <>
+            {
+            allBooks.map((book)=>
+                <div key={book.id}>
+                    <img src={book.coverimage}/>
+                    <h2>{book.title}</h2>
+                    <h3>{book.available}</h3>
+                    <button onClick={handleClick}>More details!</button>
+                </div>
+            )}
+        </>
     )
 }
