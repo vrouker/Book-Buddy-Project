260c4696ac121ef616125eaee227b1229a6f62a4
vrouker <vrouker@gmail.com>
2025-05-01 19:44:00 -0400

    Navbar is updated based on whether a user is logged in or not

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -12,14 +12,28 @@ export default function Navigations ({token, setToken}){
 
     return(
        <>
-         <Link to="/">Home</Link>
-         <Link to="/login">Login</Link>
-         <Link to="/register">Sign Up</Link>
-         <input
+       {
+            token ?
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/account">My Account</Link>
+            <input
             type="text"
             placeholder="Search here"
             />
-        <button onClick={handleSignOut}>Log Out</button>
+            <button onClick={handleSignOut}>Log Out</button>
+        </div>
+        :
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Sign Up</Link>
+            <input
+            type="text"
+            placeholder="Search here"
+            />
+        </div>
+       }
        
        </> 
     )
